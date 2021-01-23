76a841f1594de20353d7fc3e4fb784ade3f0f4a3
Arvin Fernandez <arvinf07@gmail.com>
2021-01-23 11:30:17 -0500

    some more drying

--- app/helpers/exercises_helper.rb
+++ app/helpers/exercises_helper.rb
@@ -1,17 +1,3 @@
 module ExercisesHelper
 
-  #How can I make this method return the li tags 
-  def display_steps(exercise)
-    steps = exercise.description.split("\n")
-    steps.map do |step|
-      content_tag :li, step
-    end
-
-    # content_tag(:ul) do
-    #   (1..5).to_a.map do
-    #     content_tag(:li, step_number, class: "step)
-    #   end.reduce(&:+)
-    # end
-  end
-
 end
